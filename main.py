bab7ad2ff37cb640cb036ede633e44deb4ba8727
Olga <silke0330@yandex.ru>
2024-07-10 12:26:43 +0300

    Внесены изменения в main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #Подключаем библиотек для создания игры
 import pygame
+import requests
+import rembg
+from io import BytesIO
 
 #Инизализируем модуль игры
 pygame.init()
@@ -11,10 +14,94 @@ HEIGHT = 800
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Two-Player Chess Game')
 
+#Устанавливаем шрифты
 font = pygame.font.Font('freesansbold.ttf', 20)
 medium_font = pygame.font.Font('freesansbold.ttf', 40)
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 
+"""Создание таймера и кадров в секунду"""
+timer = pygame.time.Clock()
+fps = 60
+
+"""Созадние фигур и их позиции"""
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
+
+captured_pieces_white = []
+captured_pieces_black = []
+
+turn_step = 0
+selection = 100
+valid_moves = []
+
+image_urls = ['https://media.geeksforgeeks.org/wp-content/uploads/20240302025946/black_queen.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025948/black_king.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025345/black_rook.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025951/black_bishop.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025947/black_knight.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025945/black_pawn.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025952/white_queen.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025943/white_king.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025949/white_rook.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025944/white_bishop.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025325/white_knight.png',
+              'https://media.geeksforgeeks.org/wp-content/uploads/20240302025953/white_pawn.png']
+
+black_queen = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[0]).content)))
+black_queen = pygame.transform.scale(black_queen, (80, 80))
+black_queen_small = pygame.transform.scale(black_queen, (45, 45))
+black_king = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[1]).content)))
+black_king = pygame.transform.scale(black_king, (80, 80))
+black_king_small = pygame.transform.scale(black_king, (45, 45))
+black_rook = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[2]).content)))
+black_rook = pygame.transform.scale(black_rook, (80, 80))
+black_rook_small = pygame.transform.scale(black_rook, (45, 45))
+black_bishop = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[3]).content)))
+black_bishop = pygame.transform.scale(black_bishop, (80, 80))
+black_bishop_small = pygame.transform.scale(black_bishop, (45, 45))
+black_knight = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[4]).content)))
+black_knight = pygame.transform.scale(black_knight, (80, 80))
+black_knight_small = pygame.transform.scale(black_knight, (45, 45))
+black_pawn = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[5]).content)))
+black_pawn = pygame.transform.scale(black_pawn, (65, 65))
+black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
+white_queen = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[6]).content)))
+white_queen = pygame.transform.scale(white_queen, (80, 80))
+white_queen_small = pygame.transform.scale(white_queen, (45, 45))
+white_king = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[7]).content)))
+white_king = pygame.transform.scale(white_king, (80, 80))
+white_king_small = pygame.transform.scale(white_king, (45, 45))
+white_rook = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[8]).content)))
+white_rook = pygame.transform.scale(white_rook, (80, 80))
+white_rook_small = pygame.transform.scale(white_rook, (45, 45))
+white_bishop = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[9]).content)))
+white_bishop = pygame.transform.scale(white_bishop, (80, 80))
+white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
+white_knight = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[10]).content)))
+white_knight = pygame.transform.scale(white_knight, (80, 80))
+white_knight_small = pygame.transform.scale(white_knight, (45, 45))
+white_pawn = pygame.image.load(
+    BytesIO(rembg.remove(requests.get(image_urls[11]).content)))
+white_pawn = pygame.transform.scale(white_pawn, (65, 65))
+white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
 
 #Выходим из модуля игры
 pygame.quit()
